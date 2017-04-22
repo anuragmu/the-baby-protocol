863fc4e11ef06a2d33f28c4690e46377a2c9320f
Anurag <anuragmundhada@gmail.com>
2017-04-22 22:42:15 +0530

    service file added

---
+++ service.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import pyaudio, os
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source) # listen for 1 second to calibrate the energy threshold for ambient noise levels
+    print("Say something!")
+    audio = r.listen(source)
+
+
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
